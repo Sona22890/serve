fa46c22c31e275f3ac4c4907a1ca26d9c29a3564
Sona22890 <sonakanwar8302@gmail.com>
2024-12-10 13:10:21 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7836136897:AAG-0BYZKzGLp34DYSGzu95B1XQAgnlAjpc")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@MR_CHETAN_OP")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("904651079","7855020275","5759284972","7700702349")
+
+
+
+
+
+  
